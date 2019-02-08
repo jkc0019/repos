fe1c32319e8fbf0063ebe395f2fb2c31bf809988
Cobb,Joseph Kitridge <jkc0019@mix.wvu.edu>
2019-02-08 07:51:10 -0500

    UTF-8

---
+++ ApiControllers/ApiControllers/Models/Reservation.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiControllers.Models
+{
+
+	public class Reservation
+	{
+		public int ReservationId { get; set; }
+		public string ClientName { get; set; }
+		public string Location { get; set; }
+	}
+}
---
+++ Views/Views/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Views.Controllers {
+
+    public class HomeController : Controller {
+
+		public ViewResult Index()
+		{
+			ViewBag.Message = "Heya";
+			ViewBag.Time = DateTime.Now.ToShortTimeString();
+			return View("DebugData");
+		}
+
+        public ViewResult List() => View();
+    }
+}
